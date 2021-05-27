46d8b7f6608a8477781d5cd58a2620f0fb4590a6
Sainath876 <78300287+Sainath876@users.noreply.github.com>
2021-05-27 14:36:45 +0530

    Add Start Script

--- index.js
+++ index.js
@@ -169,6 +169,8 @@ app.use((err, req, res, next) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log("Connected to server port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Connected to server port ${port}`);
 });
